d575dd7ad3b80cbc81f1a204734b30400a980827
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-10-10 02:01:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function testFunc() {
 }
 
 function superbowlWin(array) {
-  let record = array.find(s => s === "W" )
+  let record = array.find( s => s === "W" )
   return record ? record.year: undefined
 }
